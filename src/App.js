1f9ad78ca65d60ccefafb90e6a7af643fd6af095
Jalilur Rahman <jrahman90.work@gmail.com>
2022-01-30 21:26:25 -0500

    working map spaces

--- src/App.js
+++ src/App.js
@@ -30,9 +30,6 @@ const App = () => {
         {/* <LogoutButton /> */}
         <NavBar />
         <Map />
-        <br />
-        <br />
-        <br />
         <BottomTab />
       </div>
     );
