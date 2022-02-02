144fa53d73d14914bf349253ba45267080dd7439
Johnny Vela <vela3733@gmail.com>
2022-02-02 11:34:03 -0800

    cleanup

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -25,14 +25,10 @@ export default function FormDialog() {
   const [postTime, setPostTime] = useState("");
 
   useEffect(() => {
-    var date = new Date().getDate(); //Current Date
-    var month = new Date().getMonth() + 1; //Current Month
-    var year = new Date().getFullYear(); //Current Year
     var hours = new Date().getHours(); //Current Hours
     var min = new Date().getMinutes(); //Current Minutes
-    var sec = new Date().getSeconds(); //Current Seconds
     setPostTime(
-      date + "/" + month + "/" + year + " " + hours + ":" + min + ":" + sec
+      hours + ":" + min + ":" + sec
     );
   }, []);
 
