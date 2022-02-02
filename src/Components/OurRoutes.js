466ca12b4f93ab6435efc7b1982a0f8f12019d86
David Hong <davidhong.9899@gmail.com>
2022-02-02 12:55:10 -0500

    temporary components created for different tabs

--- src/Components/OurRoutes.js
+++ src/Components/OurRoutes.js
@@ -2,11 +2,18 @@ import React from 'react'
 import Map from "./Map";
 import UserProfile from "./UserProfile";
 import { Routes, Route } from 'react-router-dom'
+import Search from './Search'
+import Notifications from './Notifications'
+import Settings from './Settings'
+
 const OurRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Map />} />
+        <Route path='/Search' element={<Search />} />
+        <Route path='/Notifications' element={<Notifications />} />
+        <Route path='/Settings' element={<Settings />} />
       </Routes>
     </div>
   )
