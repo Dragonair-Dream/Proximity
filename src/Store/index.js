83e9b044b4cf3fbb210ed5e10f0009f71a983955
Jorgelly Ventura <jorgelly87@gmail.com>
2022-02-02 18:29:54 -0500

    fixed some issues

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,9 +2,6 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-import { reduxFirestore, getFirestore } from "redux-firestore";
-import { firebaseConfig } from "../secrets";
-import firebaseApp from "../Services/firebase";
 import userProfileReducer from "./userProfileReducer";
 //import reducers from store here
 
