b522348b433611447e3760ecbfc250ddca6168a6
Jalilur Rahman <jrahman90.work@gmail.com>
2022-02-02 12:26:36 -0500

    working post component sending data to firestore

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,28 +1,40 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import { doc, setDoc } from '@firebase/firestore';
-import { auth, db } from '../Services/firebase';
-import { FormControl } from '@mui/material';
-
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import { doc, setDoc } from "@firebase/firestore";
+import { auth, db } from "../Services/firebase";
+import { FormControl } from "@mui/material";
 
 //     const [imageUrl, setImageurl] = useState('');
 //     const [location, setLocation] = useState('');
 //     const [description, setDescription] = useState('');
 
 export default function FormDialog() {
-  const [open, setOpen] = useState(false)
-  const [imageUrl, setImageUrl] = useState('')
-  const [caption, setCaption] = useState('')
-  const [location, setLocation] = useState('')
-  const [post, setPost] = useState(null)
+  const [open, setOpen] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+  const [caption, setCaption] = useState("");
+  const [location, setLocation] = useState("");
+  const [post, setPost] = useState(null);
+  const [postTime, setPostTime] = useState("");
+
+  useEffect(() => {
+    var date = new Date().getDate(); //Current Date
+    var month = new Date().getMonth() + 1; //Current Month
+    var year = new Date().getFullYear(); //Current Year
+    var hours = new Date().getHours(); //Current Hours
+    var min = new Date().getMinutes(); //Current Minutes
+    var sec = new Date().getSeconds(); //Current Seconds
+    setPostTime(
+      date + "/" + month + "/" + year + " " + hours + ":" + min + ":" + sec
+    );
+  }, []);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -32,52 +44,67 @@ export default function FormDialog() {
     setOpen(false);
   };
 
-  const handleSubmit = async(e) => {
-    e.preventDefault()
-    const uid = auth.currentUser.uid
-      try {
-        const post = await setDoc(doc(db,('Posts'), uid), {imageUrl: imageUrl , location: location, caption: caption}, {merge:true})
-      } catch (error) {
-        console.log(error)
-      }
-    };
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const uid = auth.currentUser.uid;
+    try {
+      const post = await setDoc(
+        doc(db, "Posts", uid),
+        {
+          imageUrl: imageUrl,
+          location: location,
+          caption: caption,
+          postTime: postTime,
+        },
+        { merge: true }
+      );
+      setOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       {/* <Button variant="outlined" onClick={handleClickOpen}>
         Create a Post
       </Button> */}
-      <Fab size="small" color="secondary" aria-label="add" onClick={handleClickOpen}>
+      <Fab
+        size="small"
+        color="secondary"
+        aria-label="add"
+        onClick={handleClickOpen}
+      >
         <AddIcon />
       </Fab>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Create a post</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            To create a post at your current location fill out the following fields.
+            To create a post at your current location fill out the following
+            fields.
           </DialogContentText>
           <TextField
             autoFocus
             margin="dense"
             id="name"
             label="Add an image Url..."
-            type="email"
+            type="imageUrl"
+            value={imageUrl}
             fullWidth
             variant="standard"
-            onChange={(e) => console.log('image', e.target.value)}
-
+            onChange={(e) => setImageUrl(e.target.value)}
           />
           <TextField
             autoFocus
             margin="dense"
             id="name"
-            label="Add a Location..."
-            type="email"
+            label="Where Are You?"
+            type="string"
+            value={location}
             fullWidth
             variant="standard"
-            onChange={(e) => console.log('location', e.target.value)}
-
+            onChange={(e) => setLocation(e.target.value)}
           />
           <TextField
             autoFocus
@@ -85,15 +112,17 @@ export default function FormDialog() {
             id="name"
             label="Add a Caption..."
             type="email"
+            value={caption}
             fullWidth
             variant="standard"
-            onChange={(e) => console.log(e.target.value)}
-
+            onChange={(e) => setCaption(e.target.value)}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button variant="contained" onClick={handleClose} onSubmit={(e) => {handleSubmit(e)}}>Create Post</Button>
+          <Button variant="contained" onClick={(e) => handleSubmit(e)}>
+            Create Post
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
