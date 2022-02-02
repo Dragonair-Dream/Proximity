03107d86e8e6bf5e86f84b04a12459ec39fe98e7
Jorgelly Ventura <jorgelly87@gmail.com>
2022-02-02 17:51:47 -0500

    made some updates with the boys

--- src/Components/Chats.js
+++ src/Components/Chats.js
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useCallback} from 'react';
 import { auth, db } from '../Services/firebase';
-import { collection, query, where, getDoc } from 'firebase/firestore';
+import { collection, query, where, getDoc, doc } from 'firebase/firestore';
 
 
 // import {}
@@ -8,12 +8,23 @@ import { collection, query, where, getDoc } from 'firebase/firestore';
 const Chats = () => {
   console.log(auth.currentUser)
 
-  useEffect(() => {
-    const
+  const Retreive = useCallback(async () => {
+    const docRef = doc(db, 'chats', 'yFCfYlthtsJ1xGRwwCfr');
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log('DOC', docSnap.data());
+    } else {
+      console('something went wrong');
+    }
+
   }, []);
+
+
+  useEffect(() => {
+    Retreive();
+  }, [Retreive]);
 
   return <div></div>;
 };
 
 export default Chats;
-
