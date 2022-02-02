edcc4e7662ca06f35cfd78964d040dd7603a8145
Jorgelly Ventura <jorgelly87@gmail.com>
2022-02-02 14:01:39 -0500

    forgot to save a file

--- src/Components/Chats.js
+++ src/Components/Chats.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { auth, db } from '../Services/firebase';
+
+const Chats = () => {
+  console.log(auth.currentUser)
+  return <div></div>;
+};
+
+export default Chats;
 
