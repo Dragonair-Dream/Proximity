c3336ce507469c5f7aa034fdc91fbbd68f7d07dc
David Hong <davidhong.9899@gmail.com>
2022-02-08 19:58:15 -0500

    created thunk, action creator, reducer for notifications

---
+++ src/Store/notificationsReducer.js
@@ -0,0 +1,28 @@
+const SET_NOTIFICATIONS = 'SET_NOTIFICATIONS'
+
+const _setNotifications = (arr) => {
+  return {
+    type: SET_NOTIFICATIONS,
+    arr
+  }
+}
+
+export const setNotifications = (arr) => {
+  return async (dispatch) => {
+    try {
+      dispatch(_setNotifications(arr))
+    } catch(err) {
+      console.log(err)
+    }
+  }
+}
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case SET_NOTIFICATIONS:
+      console.log('NOTIFICATIONS REDUCER IS BEING SET TO: ', action.arr)
+      return action.arr
+    default:
+      return state
+  }
+}
