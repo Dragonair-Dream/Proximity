bc7f8cba1a7b620a2e0c6b3a9507459418062ad0
David Hong <davidhong.9899@gmail.com>
2022-02-08 21:15:36 -0500

    removed logs from notifications.js

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -7,10 +7,8 @@ import { readAll } from '../Store/notificationsReducer'
 const Notifications = () => {
   const notificationsArr = useSelector(state => state.notifications)
   const dispatch = useDispatch()
-  console.log('NOTIFICATIONS ARRAY IN COMPONENT IS: ', notificationsArr)
 
   useEffect(() => {
-    console.log('NOTIFICATIONS USE EFFECT RAN')
     dispatch(readAll())
   }, [])
 
